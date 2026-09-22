d0d1d011def7aef62f23d0c4a1eac846660784fe
Robosword <robotanmol@gmail.com>
2026-09-21 22:55:16 -0700

    Update client.py

--- client/client.py
+++ client/client.py
@@ -1,1 +1,15 @@
+import socket
 
+class ClientHandler:
+    def __init__(self, port):
+        self.FORMAT = 'utf-8'
+        self.DISCONNECT_MESSAGE = '!DISCONNECT'
+        self.PORT = port
+        self.SERVER = ""
+        self.ADDR = (self.SERVER, self.PORT)
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect(self.ADDR)
+
+    def send(self, msg):
+        message = msg.encode(self.FORMAT)
+        self.client.send(message)
